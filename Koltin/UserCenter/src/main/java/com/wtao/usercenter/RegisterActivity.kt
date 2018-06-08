fa203e91f45deb4a2be99d8c2f0ca344533a14d3
wangtao07@daojia.com <wangtao07@daojia.com>
2018-06-08 13:29:52 +0800

    first commit

---
+++ Koltin/UserCenter/src/main/java/com/wtao/usercenter/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.wtao.usercenter
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
