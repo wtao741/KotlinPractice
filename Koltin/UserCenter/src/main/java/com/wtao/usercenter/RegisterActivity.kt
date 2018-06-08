cc62649cf73cd589dc834d098bd51770a07e7cfc
wangtao07@daojia.com <wangtao07@daojia.com>
2018-06-08 13:50:01 +0800

    添加Kotlin-extensions

--- Koltin/UserCenter/src/main/java/com/wtao/usercenter/RegisterActivity.kt
+++ Koltin/UserCenter/src/main/java/com/wtao/usercenter/RegisterActivity.kt
@@ -2,11 +2,17 @@ package com.wtao.usercenter
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        tvRegister.setOnClickListener {
+            Toast.makeText(this,"注册",Toast.LENGTH_LONG).show()
+        }
     }
 }
